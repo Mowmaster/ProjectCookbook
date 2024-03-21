c5357116e25f60f79ee5c74bf6e1f8866f763b95
Mowmaster <mowmaster@outlook.com>
2024-03-21 08:21:16 -0500

    Initial Setup

---
+++ src/main/java/com/mowmaster/projectcookbook/util/References.java
@@ -0,0 +1,6 @@
+package com.mowmaster.projectcookbook.util;
+
+public class References {
+	public static final String MODID = "projectcookbook";
+	public static final String MODNAME = "Project Cookbook";
+}
